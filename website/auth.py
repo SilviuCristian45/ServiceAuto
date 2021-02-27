e3a856f16b173a709183a4c3227ce5e182fea071
DESKTOP-LDQLJQC\Silviu <silviudinca412@gmail.com>
2021-02-27 19:24:41 +0200

    production#10

--- website/auth.py
+++ website/auth.py
@@ -42,7 +42,7 @@ def register():
                         flash('Te rugam sa confirmi email-ul pentru a-ti activa contul.')
                         return redirect('/activateaccount')
                     except Exception as e:
-                        print("!!! " + e)
+                        print("!!! " + str(e))
                         flash('Problema la crearea contului. Contactati suport it ')
                         return redirect('/register')
                 else:
